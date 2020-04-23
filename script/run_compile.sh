b8b747a6466777acf18f7564872dc74fa6305081
caizt <caizt16@mails.tsinghua.edu.cn>
2020-04-23 19:28:35 +0800

    script

--- script/run_compile.sh
+++ script/run_compile.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 OBJ='../obj'
 rm -r $OBJ
 mkdir $OBJ && cd $OBJ
-../configure --prefix=/users/caizht/panda --enable-flopoco --enable-icarus --enable-verilator --enable-opt  --enable-debug --enable-fast-install CXX=g++-5 CC=gcc-5 CFLAGS=-pg CXXFLAGS=-pg
-#../configure --prefix=/users/caizht/panda --enable-flopoco --enable-icarus --enable-verilator --enable-opt  --enable-debug CXX=g++-5 CC=gcc-5
+../configure --prefix=/users/caizht/panda --enable-flopoco --enable-eucalyptus --enable-icarus --enable-verilator --enable-opt  --enable-debug --enable-fast-install CXX=g++-5 CC=gcc-5
 make -j8 && make -j8 install
 make -j8 documentation
